748bb09e5a82325a0edb87fc6eba2b37b71b79c0
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-01-05 10:10:25 -0500

    updated connection.rb for DO

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,6 +1,11 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection ({
-	:adapter => "postgresql",
-	:database => "museic"
-})
+ActiveRecord::Base.establish_connection ('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/museic')
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+
+# when accessing locally
+# ActiveRecord::Base.establish_connection ({
+# 	:adapter => "postgresql",
+# 	:database => "museic"
+# })
