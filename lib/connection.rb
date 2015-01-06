6c28456434e2c1da386f69bfc6d0f1ebc9992502
davidcarlsonberg <davidcarlsonberg@users.noreply.github.com>
2015-01-06 13:36:21 -0500

    styling

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 
-#ActiveRecord::Base.establish_connection ('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/museic')
+ActiveRecord::Base.establish_connection ('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/museic')
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 
